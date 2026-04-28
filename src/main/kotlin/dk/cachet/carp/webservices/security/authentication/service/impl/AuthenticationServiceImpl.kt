57159c8a23ab36b551b05f99961620a5f70ad7a1
Yuan Chen <godec52217@gmail.com>
2026-04-28 15:55:46 +0200

    Fix detekt

--- src/main/kotlin/dk/cachet/carp/webservices/security/authentication/service/impl/AuthenticationServiceImpl.kt
+++ src/main/kotlin/dk/cachet/carp/webservices/security/authentication/service/impl/AuthenticationServiceImpl.kt
@@ -101,7 +101,8 @@ class AuthenticationServiceImpl(
                         }
                 }
 
-                else -> userClaims
+                else ->
+                    userClaims
                     .filterIsInstance(claim.java)
             }
         }
